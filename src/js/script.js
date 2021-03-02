10cbd1d50016a3a6ad8a4a0e3c75f975f389cada
Vivek Verma <virtualiz.me@gmail.com>
2021-03-03 02:34:13 +0530

    Update script.js

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,5 @@
 if(window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches){
-    document.documentElement.style.setProperty('--color_background','#444');
+    document.documentElement.style.setProperty('--color_background','#282828');
     document.documentElement.style.setProperty('--color_primary','#EFC6BE');
     document.documentElement.style.setProperty('--color_text','#FAFAFA');
     document.documentElement.style.setProperty('--color_light_primary','#634A45');
